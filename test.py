bfc713943a47b6afe248e5cc4115ebaf6d5e8721
shaktirupadash <awesomeshakti13@gmail.com>
2025-12-05 18:34:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("⚡ Shakti-AI ⚡")
 st.write("Welcome to my first Streamlit app!")
 
-name = st.text_input("What's your name?")
+name = st.text_input("What's your name my dear?")
 if name:
     st.write(f"Hello, {name}! 👋")
